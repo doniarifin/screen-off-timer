52831a66c25b3f8c422f54dd6f4f6058bac24623
dhoni <muh.dhoni@gmail.com>
2026-05-03 13:58:56 +0700

    update versioning and license

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 35
         versionCode = 1
-        versionName = "1.1.4"
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
