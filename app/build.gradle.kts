0c854619a3819823958fe3a9a8e7dc04cc7443e8
doniarifin <muh.dhoni@gmail.com>
2026-06-25 16:20:22 +0700

    - fix toggle switch lockscreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2"
+        versionCode = 5
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
