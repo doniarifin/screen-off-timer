f517676c07e760eada426642aafeb5c7a2f7c120
Dhoni <muh.dhoni@gmail.com>
2026-07-20 10:16:14 +0700

    update: bump version to 14.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.3.8"
+        versionCode = 14
+        versionName = "1.3.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
